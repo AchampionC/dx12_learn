10c337812dff919128120f9a361cc21779a5d8a1
zhaohelei <zhaohelei@linctex.com>
2022-03-03 19:46:21 +0800

    DX12初始化篇：重构代码

---
+++ dx12/dx12/D3D12App.h
@@ -0,0 +1,64 @@
+#pragma once
+
+#include "ToolFunc.h"
+#include "GameTime.h"
+#include "d3dx12.h"
+
+using namespace Microsoft::WRL;
+
+class D3D12App
+{
+protected:
+	D3D12App();
+	virtual ~D3D12App();
+
+public:
+	int Run();
+	bool Init(HINSTANCE hInstance, int nShowCmd);
+	bool InitWindow(HINSTANCE hInstance, int nShowCmd);
+	bool InitDirect3D();
+	virtual void Draw() = 0;
+
+	void CreateDevice();
+	void CreateFence();
+	void GetDescriptorSize();
+	void SetMSAA();
+	void CreateCommandObject();
+	void CreateSwapChain();
+	void CreateDescriptorHeap();
+	void CreateRTV();
+	void CreateDSV();
+	void CreateViewPortAndScissorRect();
+
+	void FlushCmdQueue();
+	void CalculateFrameState();
+
+protected:
+	HWND mhMainWnd = 0;
+
+	//指针接口和变量声明
+	ComPtr<ID3D12Device> d3dDevice;
+	ComPtr<IDXGIFactory4> dxgiFactory;
+	ComPtr<ID3D12Fence> fence;
+	ComPtr<ID3D12CommandAllocator> cmdAllocator;
+	ComPtr<ID3D12CommandQueue> cmdQueue;
+	ComPtr<ID3D12GraphicsCommandList> cmdList;
+	ComPtr<ID3D12Resource> depthStencilBuffer;
+	ComPtr<ID3D12Resource> swapChainBuffer[2];
+	ComPtr<IDXGISwapChain> swapChain;
+	ComPtr<ID3D12DescriptorHeap> rtvHeap;
+	ComPtr<ID3D12DescriptorHeap> dsvHeap;
+
+	D3D12_VIEWPORT viewPort;
+	D3D12_RECT scissorRect;
+
+	UINT rtvDescriptorSize = 0;
+	UINT dsvDescriptorSize = 0;
+	UINT cbv_srv_uavDescriptorSize = 0;
+
+	//GameTime类实例声明
+	GameTime gt;
+
+	UINT mCurrentBackBuffer = 0;
+	int mCurrentFence = 0;	//初始CPU上的围栏点为0
+};
